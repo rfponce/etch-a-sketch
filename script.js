d6f93b36b74476a92545dae0ac281996ba1f1eb0
Fabián Ponce <maximohumano@gmail.com>
2022-09-16 10:18:52 -0500

    Create a function to generate a square in #canvas

---
+++ script.js
@@ -0,0 +1,10 @@
+// Create a div and show it to canvas
+function generateSquare(sizeInPx) {
+  const canvas = document.getElementById('canvas');
+  let square = document.createElement('div');
+
+  square.style.width = sizeInPx;
+  square.style.height = sizeInPx;
+  square.style.border = "1px solid black";
+  canvas.appendChild(square);
+}
